ef28516c7b7f4883581ed3195feee6dda8db4804
Daniel <ahmagensh@gmail.com>
2026-01-06 01:45:50 +0100

    Create userProfile.js

---
+++ entities/userProfile.js
@@ -0,0 +1,16 @@
+export const UserProfile = {
+  display_name: '',
+  avatar_id: 'avatar_1',
+  fuel: 50,
+  reputation: 0,
+  faction: 'nova',
+  streak_days: 0,
+  subscription: 'free',
+  cosmetics: {
+    frame: 'none',
+    aura: 'none',
+    emotes: [],
+  },
+  current_room_id: null,
+  is_guest: true,
+};
